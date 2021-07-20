5046d378a3cfafa3322c01fe8ccc0ecbe44b571a
ivanarojas <ivana.rojas@hotmail.com>
2021-07-20 17:17:07 -0300

    generacion de componentes

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { QuienesSomosComponent } from './quienes-somos/quienes-somos.component';
+import { RegistroComponent } from './registro/registro.component';
+import { IniciarSesionComponent } from './iniciar-sesion/iniciar-sesion.component';
+
+
+
+@NgModule({
+  declarations: [
+    ServiciosComponent,
+    QuienesSomosComponent,
+    RegistroComponent,
+    IniciarSesionComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PagesModule { }
