f4a7c959e09a7eb493aa66af8f1fd7b3bf6ed16a
ivanarojas <ivana.rojas@hotmail.com>
2021-07-20 22:17:14 -0300

    Sistema de rutas en angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IniciarSesionComponent } from './pages/iniciar-sesion/iniciar-sesion.component';
+import { QuienesSomosComponent } from './pages/quienes-somos/quienes-somos.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { ServiciosComponent } from './pages/servicios/servicios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'inicio', component:ServiciosComponent},
+  {path: 'quienes-somos', component: QuienesSomosComponent},
+  {path: 'registro', component:RegistroComponent},
+  {path: 'iniciar-sesion', component: IniciarSesionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
